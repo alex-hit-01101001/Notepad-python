d73cd2a62681862d7d01dd082b068e6b5160e825
alex-hit-01101001 <98972898+alex-hit-01101001@users.noreply.github.com>
2022-06-23 12:09:39 +0300

    Main program
    Uploaded main notepad program

---
+++ notepad.py
@@ -0,0 +1,111 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+import os
+
+# Functions
+def new():
+    inpu1 = T.get("1.0", 'end-1c')
+    if inpu1 != "" or saved != True:
+        new_root = Tk()
+        new_root.geometry("200x100")
+        new_root.title("")
+        label_ask = Label(new_root, text="are you sure ?")
+        label_ask.pack(expand=True)
+        button_yes = Button(new_root, text="yes")
+        button_yes.pack(side="left")
+        button_no = Button(new_root, text="no")
+        button_no.pack(side="right")
+
+"""
+def save2():
+    def save():
+        try:
+            text_file = open("untitled.txt", "r")
+            text_file.close()
+        except FileNotFoundError:
+            saved = True
+            text_file = open("untitled.txt", "w")
+            inpu = T.get("1.0", 'end-1c')
+            text_file.write(inpu)
+
+    if saved == False:
+        save()
+    else:
+        try:
+            text_file = open("untitled.txt", "r")
+            text_file.close()
+            print("exist")
+        except FileNotFoundError:
+            print("not")
+"""
+
+
+def save():
+    i = 0
+    i = str(i)
+    name = "untitled", i, ".txt"
+    name = ''.join(name)
+    try:
+        text_file = open(name, "r")
+        text_file.close()
+    except FileNotFoundError:
+        saved = True
+        text_file = open(name, "w")
+        inpu = T.get("1.0", 'end-1c')
+        text_file.write(inpu)
+        i = int(i)
+        i = i + 1
+
+def save_as():
+    filename = filedialog.askopenfilename(initialdir="/", title="Select a File",
+                                          filetypes=(("Text files", "*.txt*"), ("all files", "*.*")))
+    print(os.path.dirname(filename))
+
+
+def open1():
+    filename = filedialog.askopenfilename(initialdir="/", title="Select a File",
+                                          filetypes=(("Text files", "*.txt*"), ("all files", "*.*")))
+    text_file = open(filename, "r")
+    content = text_file.read()
+    text_file.close()
+    T.clipboard_clear()
+    T.insert(INSERT, content)
+    
+    
+# Main
+root = Tk()
+root.geometry("800x600")
+root.title("Notepad")
+saved = False
+S = tk.Scrollbar(root)
+T = tk.Text(root, height=4, width=50)
+S.pack(side=tk.RIGHT, fill=tk.Y)
+T.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+S.config(command=T.yview)
+T.config(yscrollcommand=S.set)
+
+# TOP BAR MENU
+menu1 = Menu(root)
+root.config(menu=menu1)
+# NEW
+File_menu = Menu(menu1)
+menu1.add_cascade(label="File", menu=File_menu)
+File_menu.add_command(label="New", command=new)
+File_menu.add_command(label="Open", command=open1)
+File_menu.add_command(label="Save", command=save)
+File_menu.add_command(label="Save as", command=save_as)
+# EDIT
+Edit_menu = Menu(menu1)
+menu1.add_cascade(label="Edit", menu=Edit_menu)
+Edit_menu.add_command(label="Preferences", command="")
+# SETTINGS
+Settings_menu = Menu(menu1)
+menu1.add_cascade(label="Settings", menu=Settings_menu)
+Settings_menu.add_command(label="Preferences", command="")
+# HELP
+help_menu = Menu(menu1)
+menu1.add_cascade(label="Help", menu=help_menu)
+help_menu.add_command(label="About", command="")
+
+tk.mainloop()
